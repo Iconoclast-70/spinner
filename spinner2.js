07adcae14046a853d1ecc70cac4b0b7130dcf7a1
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-06-01 21:45:33 +0000

    spinner 2

---
+++ spinner2.js
@@ -0,0 +1,10 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+const spin = "|/-\\||/-\\|\n";
+let animate = 0;
+for (const char of spin) {
+  animate += 200;
+  setTimeout(() => {
+    process.stdout.write("\r" + char);
+  }, 100 + animate);
+}; 
